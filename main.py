b116567105834fc2d55a629267d80a3c5a2bd233
Johann Schneider <joschi.dev7@gmail.com>
2025-06-25 15:47:04 +0200

    a function, which counts every letter, has been added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words 
+from stats import count_characters
 
 def get_book_test(path):
     content = ""
@@ -12,6 +13,10 @@ def main():
     path_to_file = "./books/frankenstein.txt"
     book_content = get_book_test(path_to_file)
     word_count = count_words(book_content)
+    characters_count = count_characters(book_content)
     print(f"{word_count} words found in the document")
+    #count_characters(book_content)
+    for c in characters_count:
+        print(f"'{c}': {characters_count[c]}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,16 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    words = text.split()
+    counter = {}
+    for w in words:
+        for l in w:
+            #print(l)
+            new_l = l.lower()
+            if new_l in counter:
+                counter[new_l] += 1
+            else:
+                counter[new_l] = 1
+    return counter
