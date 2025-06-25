dd92f0a6473b04a390a2a3d725be1b969c1d5c9e
Johann Schneider <joschi.dev7@gmail.com>
2025-06-25 15:34:42 +0200

    count_words func has been outsourced in its own file

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+from stats import count_words 
+
+def get_book_test(path):
+    content = ""
+
+    with open(path) as f:
+        content = f.read()
+
+    return content
+
+def main():
+    path_to_file = "./books/frankenstein.txt"
+    book_content = get_book_test(path_to_file)
+    word_count = count_words(book_content)
+    print(f"{word_count} words found in the document")
+
+main()
